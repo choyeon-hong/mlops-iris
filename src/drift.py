0ba2aca207c03f6412f68d39d8a83630947d72ed
choyeon-hong <79977896+choyeon-hong@users.noreply.github.com>
2026-02-28 17:48:51 +0900

    test the drift trigger7

--- src/drift.py
+++ src/drift.py
@@ -1,20 +1,28 @@
 import pandas as pd
-import json
-
 from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset
 
 ref = pd.read_csv("data/train.csv")
 new = pd.read_csv("data/new_data.csv")
 
+ref = ref.drop(columns=["target"])
+new = new.drop(columns=["target"])
+
 report = Report(metrics=[DataDriftPreset()])
 report.run(reference_data=ref, current_data=new)
 
 result = report.as_dict()
 
-drift = result["metrics"][0]["result"]["dataset_drift"]
+feature_metrics = result["metrics"][0]["result"]["drift_by_columns"]
 
-print("drift:", drift)
+drift_detected = False
 
-if drift:
+for k, v in feature_metrics.items():
+    print(k, v["drift_detected"])
+    if v["drift_detected"]:
+        drift_detected = True
+
+print("feature drift:", drift_detected)
+
+if drift_detected:
     exit(1)
