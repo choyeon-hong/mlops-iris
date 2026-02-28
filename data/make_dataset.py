cb87e24427368c5f0effde022d442e8b55b2136d
choyeon-hong <79977896+choyeon-hong@users.noreply.github.com>
2026-02-28 17:41:56 +0900

    test the drift trigger5

--- data/make_dataset.py
+++ data/make_dataset.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_iris
 import pandas as pd
+import numpy as np
+
 from sklearn.model_selection import train_test_split
 
 iris = load_iris()
@@ -15,5 +17,12 @@ train, new = train_test_split(df, test_size=0.2, random_state=42)
 
 train.to_csv("data/train.csv", index=False)
 new.to_csv("data/new_data.csv", index=False)
+
+df = pd.read_csv("data/new_data.csv")
+
+df["f1"] = np.random.normal(50, 10, size=len(df))
+df["f2"] = np.random.normal(50, 10, size=len(df))
+
+df.to_csv("data/new_data.csv", index=False)
 
 print("dataset created")
